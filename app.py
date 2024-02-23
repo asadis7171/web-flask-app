46779fa6874c8d66cab0a8e92d36072013320792
root <root@Asad-PC>
2024-02-23 13:17:24 +0530

    added new files

--- app.py
+++ app.py
@@ -10,5 +10,9 @@ def main():
 def hello():
     return 'I am good, how about you?'
 
+@app.route('/Who are you')
+def cloudethix():
+    return 'Yes, I am cloudethix, and You !!!'
+
 if __name__ == "__main__":
     app.run()
